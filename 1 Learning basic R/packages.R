bd395e57cfa647196739f99ee814aa6e2fd5898f
dessei <Fritz.Meitner@gmx.de>
2022-03-12 00:59:21 +0100

    putting in packages.r (might be renamed later -> in depth focus on ggplot(2))

---
+++ 1 Learning basic R/packages.R
@@ -0,0 +1,8 @@
+#packages only need to be installed once
+#example:
+# install.packages("ggplot2") #was run in console
+
+library(ggplot2) #to use it in the actual code
+
+#the real fun is now looking through the documentation to search for what
+#you're actually looking for in the current context of the objective
